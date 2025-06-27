37d40b9c91c7fe126bfa368c7e2b80d09ce0e850
Garrett <149735110+malcus-g@users.noreply.github.com>
2025-06-26 21:40:40 -0400

    stage 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Main {
@@ -7,12 +8,21 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            String[] line = input.split(" ");
+            String command = line[0];
 
-            switch (input){
-                case "exit 0":
+            switch (command){
+                case "exit":
                     return;
+                case "echo":
+                    StringBuilder text = new StringBuilder(" ");
+                    for (int i = 1; i < line.length; i++) {
+                        text.append(line[i]).append(" ");
+                    }
+                    System.out.println(text);
+                    break;
                 default:
-                    System.out.println(input + ": command not found");
+                    System.out.println(command + ": command not found");
             }
         }
     }
